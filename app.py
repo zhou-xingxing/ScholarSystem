051e8f1f91075e8683e44f604c69d53213c1f36d
Bruce-yu <1178141259@qq.com>
2020-02-29 17:24:45 +0800

    Project Structre

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
-from flask import Flask
+from flask import Flask,render_template
+
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    print('申林-测试pycharm')
-    print('DMY-Test')
-    print("bwm")
-    return '高校学者发现系统!'
+def index():
+    return  render_template("index.html")
 
 
 if __name__ == '__main__':

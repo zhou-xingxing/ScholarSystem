470b573c5175bb4690f6013c89d5ec201c0f7948
zhou-xingxing <xmz@cau.edu.cn>
2020-03-01 20:30:58 +0800

    RelationNet_test.html

--- app.py
+++ app.py
@@ -19,8 +19,9 @@ def feedback():
 # 处理反馈
 @app.route('/handlefeedback', methods=['GET', 'POST'])
 def handlefeedback():
-    # 反馈学者姓名，反馈类型，反馈内容，邮箱，是否接受反馈，当前时间
+    # 反馈学者姓名，学术机构、反馈类型，反馈内容，邮箱，是否接受反馈，当前时间
     name = request.form.get('name')
+    school = request.form.get('school')
     type = request.form.get('type')
     content = request.form.get('content')
     email = request.form.get('email')
@@ -30,12 +31,16 @@ def handlefeedback():
     else:
         receive = False
     time = datetime.datetime.now()
-    print(name, type, content, email, receive, time)
+    print(name, school,type, content, email, receive, time)
     # FeedBack接口待实现
-    # SuccessOrNot=FeedBack(name,type,content,email,receive,time)
+    # SuccessOrNot=FeedBack(name,school,type,content,email,receive,time)
     SuccessOrNot = True
     return render_template("feedback_result.html",SuccessOrNot=SuccessOrNot)
 
+# 测试关系网络图
+@app.route('/relation')
+def relation():
+    return render_template("RelationNet_test.html")
 
 if __name__ == '__main__':
     app.run()
